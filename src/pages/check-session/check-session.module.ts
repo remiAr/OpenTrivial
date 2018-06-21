3a36feabc3cbf56034e529ada61c41ffe635efd9
remi arminio <remi.arminio@gmail.com>
2018-06-21 10:03:50 +0200

    add login page & checkSession page & native storage

---
+++ src/pages/check-session/check-session.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CheckSessionPage } from './check-session';
+
+@NgModule({
+  declarations: [
+    CheckSessionPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CheckSessionPage),
+  ],
+})
+export class CheckSessionPageModule {}
