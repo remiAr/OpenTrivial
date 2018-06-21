9b0f2c5f9c3e5dd8292f393d3725e88d8f4b2c96
remi arminio <remi.arminio@gmail.com>
2018-06-21 10:49:21 +0200

    login/deco et native storage

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 /**
  * Generated class for the LoginPage page.
@@ -10,19 +12,62 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+	selector: 'page-login',
+	templateUrl: 'login.html',
 })
 export class LoginPage {
 
-  avatar: string;
+	avatar: string;
+	userName: string;
+	userSeted: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.avatar = "";
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams, private nativeStorage: NativeStorage, private loadingCtrl: LoadingController) {
+		this.avatar = "";
+		this.userName = "";
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+	ionViewDidLoad() {
+		const loader = this.loadingCtrl.create({
+			content: "Please wait..."
+		});
+		loader.present().then(() => {
+			this.nativeStorage.getItem('myUser')
+				.then(
+					data => {
+						this.userSeted = true;
+						alert(JSON.stringify(data));
+						loader.dismiss();
+					},
+					error => console.error(error)
+				);
+		});
+	}
+
+	setUser(username) {
+		const loader = this.loadingCtrl.create({
+			content: "Please wait..."
+		});
+		loader.present().then(() => {
+			/* this.nativeStorage.getItem('myUser')
+				.then(
+					data => console.log(data),
+					error => console.error(error)
+				); */
+			this.nativeStorage.setItem('myUser', { userName: username })
+				.then(
+					() =>  {
+						loader.dismiss();
+						this.userSeted = true;
+					},
+					error => console.error('Error storing item', error)
+				);
+		});
+
+
+	}
+	deconnection(){
+		
+	}
+
 
 }
--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -11,7 +11,11 @@ import { Injectable } from '@angular/core';
 export class RestProvider {
 
   constructor(public http: HttpClient) {
-    console.log('Hello RestProvider Provider');
+    
+  }
+
+  getQuestion(){
+    
   }
 
 }
